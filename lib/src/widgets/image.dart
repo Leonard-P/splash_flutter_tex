238f7b4073fb21ced8a85dd3e2c17b237596c31a
Jonas-Thumbs <jonas.thumbs@gmail.com>
2025-03-13 10:23:29 +0100

    add TeXViewImage.file() constructor

--- lib/src/widgets/image.dart
+++ lib/src/widgets/image.dart
@@ -1,6 +1,8 @@
+import 'dart:io';
 import 'package:flutter_tex/flutter_tex.dart';
 import 'package:flutter_tex/src/models/widget_meta.dart';
 import 'package:flutter_tex/src/utils/style_utils.dart';
+import 'dart:convert'; // Import dart:convert for base64
 
 class TeXViewImage extends TeXViewWidget {
   /// Uri for Image.
@@ -11,6 +13,15 @@ class TeXViewImage extends TeXViewWidget {
   const TeXViewImage.asset(this.imageUri) : _type = 'tex-view-asset-image';
 
   const TeXViewImage.network(this.imageUri) : _type = 'tex-view-network-image';
+
+  TeXViewImage.file(File imageFile)
+      : imageUri = _fileToBase64(imageFile),
+        _type = 'tex-view-file-image';
+
+  static String _fileToBase64(File imageFile) {
+    List<int> imageBytes = imageFile.readAsBytesSync();
+    return 'data:image/png;base64,${base64Encode(imageBytes)}';
+  }
 
   @override
   TeXViewWidgetMeta meta() {
